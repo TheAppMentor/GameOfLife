c08b4c1cd4a2da4de41d0550a046eceb3724d38c
Prashanth Moorthy <prashanth.moorthy@sap.com>
2020-01-28 18:04:43 +0530

    Initial Commit

---
+++ GoL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoL
+//
+//  Created by Moorthy, Prashanth on 28/01/20.
+//  Copyright © 2020 Moorthy, Prashanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
